f025bc11ace710b4d7079cda0edaaf22818b6828
ohgus <123801385+ohgus@users.noreply.github.com>
2024-10-11 22:33:31 +0900

    [refactor]: 개별 이름에 대한 검증 로직을 분리

--- src/validator.js
+++ src/validator.js
@@ -1,21 +1,23 @@
-export const validateCarNames = (carNames) => {
+const isValidCarName = (carName) => {
   const englishRegex = /^[a-zA-Z]+$/;
 
+  if (!englishRegex.test(carName)) {
+    throw new Error(
+      '[ERROR] 차 이름은 영어로만 입력해주세요. 공백과 특수문자도 포함할 수 없습니다.'
+    );
+  }
+
+  if (carName.length < 2 || carName.length > 5) {
+    throw new Error('[ERROR] 차 이름은 2자 이상 5자 이하로 입력해주세요.');
+  }
+};
+
+export const validateCarNames = (carNames) => {
   if (carNames.length < 2 || carNames.length > 5) {
     throw new Error('[ERROR] 차 이름은 2개 이상 5개 이하로 입력해주세요.');
   }
 
-  for (const carName of carNames) {
-    if (!englishRegex.test(carName)) {
-      throw new Error(
-        '[ERROR] 차 이름은 영어로만 입력해주세요. 공백과 특수문자도 포함할 수 없습니다.'
-      );
-    }
-
-    if (carName.length < 2 || carName.length > 5) {
-      throw new Error('[ERROR] 차 이름은 2자 이상 5자 이하로 입력해주세요.');
-    }
-  }
+  carNames.forEach(isValidCarName);
 
   if (carNames.length !== new Set(carNames).size) {
     throw new Error('[ERROR] 차 이름은 중복될 수 없습니다.');
